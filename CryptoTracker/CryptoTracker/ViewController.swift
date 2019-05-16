0699c099aab40e733e0990c57a39beb62d14ad23
Alexander Schüßling <alex-schuessling@web.de>
2019-05-16 16:20:39 +1000

    Project setup

---
+++ CryptoTracker/CryptoTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Alexander Schüßling on 16.05.19.
+//  Copyright © 2019 UTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
