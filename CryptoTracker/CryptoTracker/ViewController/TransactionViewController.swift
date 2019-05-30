c295352dd5f7eac01bd69855f75abd1cbbe331eb
Mahinda <aggajoti@gmail.com>
2019-05-30 11:19:22 +1000

    fixes the problems with the textfield on transactions (Re-commit)
    Now the two textfields (amount and quantity) will properly update when one has been given an input, instead of returning as just 0.00
    
    Re-commiting as my previous commit had me accidentally commit a whole zip file.

--- CryptoTracker/CryptoTracker/ViewController/TransactionViewController.swift
+++ CryptoTracker/CryptoTracker/ViewController/TransactionViewController.swift
@@ -20,6 +20,9 @@ class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPicke
     
     var q = 0.0
     var c = ""
+    var amountTimer = Timer()
+    var quantityTimer = Timer()
+    var a = 0.0
     
     //String(format: "$%.02f", currencies[c]!)
     @IBAction func amountAction(_ sender: UITextField) {
@@ -29,7 +32,7 @@ class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPicke
         q = newQ
         c = coin
         print(String(newQ))
-        quantityText.text = String(format: "%.02f", String(newQ))
+        quantityText.text = String(format: "%.02f", newQ)
     }
     
     @IBAction func quantityAction(_ sender: UITextField) {
@@ -37,9 +40,10 @@ class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPicke
         let coin: String = currString[coinPicker.selectedRow(inComponent: 0)]
         let value: Double = currencies[coin]!
         let newV: Double = Double(self.quantityText.text!)! * value
+        a = newV
         c = coin
         print(newV)
-        amountText.text = String(format: "%.02f", String(newV))
+        amountText.text = String(format: "%.02f", newV)
     }
     
     @IBAction func saveButton(_ sender: UIButton) {
@@ -68,6 +72,8 @@ class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPicke
         amountText.text = "123"
         super.viewDidLoad()
         setupView()
+        //runAmountUpdater()
+        //runQuantityUpdater()
     }
     
     func setupView() {
@@ -76,5 +82,29 @@ class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPicke
         coinPicker.layer.cornerRadius = 5
         saveButtonOutlet.layer.cornerRadius = 5
     }
-
+    
+    
+    /*
+     //this is the code to update the Amount in real time (or as close to real time as possible.
+     @objc func runAmountUpdater() {
+     amountTimer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: (#selector(amountUpdater)), userInfo: nil, repeats: true)
+     }
+     
+     //this updates the amount at each time interval
+     @objc func amountUpdater() {
+     print(String(a))
+     //amountText.text = String(format: "%.02f", a)
+     }
+     
+     //this is the code to update the Quantity in real time (or as close to real time as possible.
+     @objc func runQuantityUpdater() {
+     quantityTimer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: (#selector(quantityUpdater)), userInfo: nil, repeats: true)
+     }
+     
+     //this updates the quantity at each time interval
+     @objc func quantityUpdater() {
+     //quantityText.text = String(format: "%.02f", String(a))
+     print(String(q))
+     }
+     */
 }
